e10ecfa01842314de58db3cc72b113dd7e53f369
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-05-30 21:02:09 +0530

    Time: 5 ms (64.87%) | Memory: 15.1 MB (48.58%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -17,19 +17,51 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
+        //Brute force
+        // Node*temp=head;
+        // map<Node*,Node*>mpp;
+        // while(temp){
+        //     Node* newNode=new Node(temp->val);
+        //     mpp[temp]=newNode;
+        //     temp=temp->next;
+        // }
+        // temp=head;
+        // while(temp){
+        //     mpp[temp]->next=mpp[temp->next];
+        //     mpp[temp]->random=mpp[temp->random];
+        //     temp=temp->next;
+        // }
+        // return mpp[head];
+
+
+        //Optimal
         Node*temp=head;
-        map<Node*,Node*>mpp;
+        //insert copyNodes in between.
         while(temp){
-            Node* newNode=new Node(temp->val);
-            mpp[temp]=newNode;
-            temp=temp->next;
+            Node* nextElement=temp->next;
+            Node* cpNode=new Node(temp->val);
+            cpNode->next=nextElement;
+            temp->next=cpNode;
+            temp=nextElement;
         }
         temp=head;
+        //initialize random pointers in copy nodes.
         while(temp){
-            mpp[temp]->next=mpp[temp->next];
-            mpp[temp]->random=mpp[temp->random];
+            if(temp->random) temp->next->random=temp->random->next;
+            else    temp->next->random=nullptr;
+            temp=temp->next->next;
+        }
+        Node* dNode=new Node(-1);
+        Node* res=dNode;
+        temp=head;
+        //connecting next in both lists.
+        while(temp){
+            res->next=temp->next;
+            temp->next=temp->next->next;
+
+            res=res->next;
             temp=temp->next;
         }
-        return mpp[head];
+        return dNode->next;
     }
 };
