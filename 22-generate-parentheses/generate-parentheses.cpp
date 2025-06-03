1c9d8d4e1b5ed4303cccd8534a65ea28e3346caf
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-06-03 20:46:01 +0530

    Time: 3 ms (71.89%) | Memory: 15.6 MB (49.81%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+    void func(vector<string>&v,int open,int close,string s){
+        if(open==0){
+            while(close){
+                s+=")";
+                close--;
+            }
+            v.push_back(s);
+            return;
+        }
+
+        if(open){
+            string x=s,y=s;
+
+            x+="(";
+            ;int o=open-1;
+            func(v,o,close,x);
+
+            if(open<close){
+                y+=")";
+                int c=close-1;
+                func(v,open,c,y);
+            }
+        }
+    }
+
+
+    vector<string> generateParenthesis(int n) {
+        vector<string>v;
+        func(v,n,n,"");
+        return v;
+    }
+};
