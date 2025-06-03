534214164320c7e38ce4ec4c6a4eb678b7823898
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-06-03 20:55:26 +0530

    Time: 2 ms (82.45%) | Memory: 15.6 MB (64.43%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,33 +1,47 @@
 class Solution {
 public:
 
-    void func(vector<string>&v,int open,int close,string s){
-        if(open==0){
-            while(close){
-                s+=")";
-                close--;
-            }
-            v.push_back(s);
-            return;
-        }
+    // void func(vector<string>&v,int open,int close,string s){
+    //     if(open==0){
+    //         while(close){
+    //             s+=")";
+    //             close--;
+    //         }
+    //         v.push_back(s);
+    //         return;
+    //     }
 
-        if(open){
-            string x=s,y=s;
+    //     if(open){
+    //         string x=s,y=s;
 
-            x+="(";
-            func(v,open-1,close,x);
+    //         x+="(";
+    //         func(v,open-1,close,x);
 
-            if(open<close){
-                y+=")";
-                func(v,open,close-1,y);
-            }
-        }
+    //         if(open<close){
+    //             y+=")";
+    //             func(v,open,close-1,y);
+    //         }
+    //     }
+    // }
+
+    void backtrack(vector<string>& res, string s, int open, int close) {
+    if (open == 0 && close == 0) {
+        res.push_back(s);
+        return;
+    }
+
+    if (open > 0)
+        backtrack(res, s + "(", open - 1, close);
+
+    if (close > open)
+        backtrack(res, s + ")", open, close - 1);
     }
 
 
     vector<string> generateParenthesis(int n) {
         vector<string>v;
-        func(v,n,n,"");
+        // func(v,n,n,"");
+        backtrack(v,"",n,n);
         return v;
     }
 };
