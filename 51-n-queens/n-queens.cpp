d263304ade82f0915331b4f4e811222b69e6cfc1
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-06-10 10:37:21 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (75.32%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,45 +1,68 @@
 class Solution {
 public:
-    bool isSafe(int row,int col,vector<string>&board,int n){
-        int drow=row;
-        int dcol=col;
+    // bool isSafe(int row,int col,vector<string>&board,int n){
+    //     int drow=row;
+    //     int dcol=col;
 
-        while(row>=0 && col>=0){
-            if(board[row][col]=='Q')    return false;
-            row--;
-            col--;
-        }
+    //     while(row>=0 && col>=0){
+    //         if(board[row][col]=='Q')    return false;
+    //         row--;
+    //         col--;
+    //     }
 
-        row=drow,col=dcol;
+    //     row=drow,col=dcol;
 
-        while(col>=0){
-            if(board[row][col]=='Q')    return false;
-            col--;
-        }
+    //     while(col>=0){
+    //         if(board[row][col]=='Q')    return false;
+    //         col--;
+    //     }
 
-        row=drow,col=dcol;
+    //     row=drow,col=dcol;
 
-        while(row<n && col>=0){
-            if(board[row][col]=='Q')    return false;
-            row++;
-            col--;
-        }
+    //     while(row<n && col>=0){
+    //         if(board[row][col]=='Q')    return false;
+    //         row++;
+    //         col--;
+    //     }
 
-        return true;
+    //     return true;
 
-    }
+    // }
 
-    void solve(int col,vector<string>&board,vector<vector<string>>&ans,int n){
-        if(col==n){
+    // void solve(int col,vector<string>&board,vector<vector<string>>&ans,int n){
+    //     if(col==n){
+    //         ans.push_back(board);
+    //         return;
+    //     }
+
+    //     for(int row=0;row<n;row++){
+    //         if(isSafe(row,col,board,n)){
+    //             board[row][col]='Q';
+    //             solve(col+1,board,ans,n);
+    //             board[row][col]='.';
+    //         }
+    //     }
+    // }
+
+    void solve(int col,vector<vector<string>>&ans,vector<string>&board,int n,vector<int>&upperDiag,vector<int>&lowerDiag,vector<int>&leftRow){
+        if(col==n)  {
             ans.push_back(board);
             return;
         }
 
         for(int row=0;row<n;row++){
-            if(isSafe(row,col,board,n)){
+            if(leftRow[row]==0 && upperDiag[n-1+col-row]==0 && lowerDiag[row+col]==0){
                 board[row][col]='Q';
-                solve(col+1,board,ans,n);
+                leftRow[row]=1;
+                upperDiag[n-1+col-row]=1;
+                lowerDiag[row+col]=1;
+
+                solve(col+1,ans,board,n,upperDiag,lowerDiag,leftRow);
+
                 board[row][col]='.';
+                leftRow[row]=0;
+                upperDiag[n-1+col-row]=0;
+                lowerDiag[row+col]=0;
             }
         }
     }
@@ -47,7 +70,11 @@ public:
     vector<vector<string>> solveNQueens(int n) {
         vector<string>board(n,string(n,'.'));
         vector<vector<string>>ans;
-        solve(0,board,ans,n);
+        vector<int>upperDiag(2*n-1,0);
+        vector<int>lowerDiag(2*n-1,0);
+        vector<int>leftRow(n,0);
+
+        solve(0,ans,board,n,upperDiag,lowerDiag,leftRow);
 
         return ans;
     }
