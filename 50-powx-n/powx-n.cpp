a9fa7cadb11a8323283a6a0479dd3afa0928265e
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-06-14 13:05:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (43.07%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -2,16 +2,32 @@ class Solution {
 public:
     double myPow(double x, int n) {
         //Binary Exponentiation
-        double ans=1.0;
-        long long nn=n;
-        if(nn<0)    nn=-nn;
-        while(nn){
-            if(nn%2){
-                ans=ans*x;
-                nn=nn-1;
-            }else{
+
+        //By iteration
+        // double ans=1.0;
+        // long long nn=n;
+        // if(nn<0)    nn=-nn;
+        // while(nn){
+        //     if(nn%2){
+        //         ans=ans*x;
+        //         nn=nn-1;
+        //     }else{
+        //         x=x*x;
+        //         nn=nn/2;
+        //     }
+        // }
+        // if(n<0) ans=1.0/ans;
+        // return ans;
+        long long N=n;
+        double ans=1;
+        if(N<0) N=-N;
+        while(N){
+            if(N%2==0){
+                N/=2;
                 x=x*x;
-                nn=nn/2;
+            }else{
+                N=N-1;
+                ans*=x;
             }
         }
         if(n<0) ans=1.0/ans;
