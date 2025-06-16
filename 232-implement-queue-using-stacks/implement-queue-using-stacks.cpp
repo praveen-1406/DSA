70ca86c853b6859e3fd832b1329b5a947ae8dd41
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-06-16 12:36:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (65.09%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -1,48 +1,73 @@
 class MyQueue {
 public:
-    stack<int>q;
-    stack<int>s;
+    stack<int>input;
+    stack<int>output;
 
     MyQueue() {
         
     }
     
     void push(int x) {
-        if(q.empty())   q.push(x);
-        else{
-            while(!q.empty()){
-                int a=q.top();
-                q.pop();
-                s.push(a);
-            }
-            q.push(x);
-            while(!s.empty()){
-                int a=s.top();
-                s.pop();
-                q.push(a);
-            }
-        }
+        // if(q.empty())   q.push(x);
+        // else{
+        //     while(!q.empty()){
+        //         int a=q.top();
+        //         q.pop();
+        //         s.push(a);
+        //     }
+        //     q.push(x);
+        //     while(!s.empty()){
+        //         int a=s.top();
+        //         s.pop();
+        //         q.push(a);
+        //     }
+        // }
+        input.push(x);
     }
     
     int pop() {
-        int x;
-        if(!q.empty()){
-            x=q.top();
-            q.pop();
-        }else   x=-1;
+        int x=-1;
+        // if(!q.empty()){
+        //     x=q.top();
+        //     q.pop();
+        // }else   x=-1;
+        // return x;
+        if(output.empty()){
+            while(!input.empty()){
+                int a=input.top();
+                output.push(a);
+                input.pop();
+            }
+        }
+        if(!output.empty()){
+            x=output.top();
+            output.pop();
+        }
+        
         return x;
     }
     
     int peek() {
-        int x;
-        if(!q.empty()){
-            x=q.top();
-        }else   x=-1;
+        int x=-1;
+        // if(!q.empty()){
+        //     x=q.top();
+        // }else   x=-1;
+        // return x;
+        if(output.empty()){
+            while(!input.empty()){
+                int a=input.top();
+                output.push(a);
+                input.pop();
+            }
+        }
+        if(!output.empty()){
+            x=output.top();
+        }
         return x;
     }
     
     bool empty() {
-        return q.empty();
+        return input.empty() && output.empty() ;
     }
 };
 
