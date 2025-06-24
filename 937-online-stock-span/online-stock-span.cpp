6aa2ecf72bfeda866a0c3c2383404ed566ca6937
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-06-24 15:38:50 +0530

    Time: 22 ms (88.34%) | Memory: 92 MB (58.36%) - LeetSync

--- 937-online-stock-span/online-stock-span.cpp
+++ 937-online-stock-span/online-stock-span.cpp
@@ -1,17 +1,25 @@
 class StockSpanner {
 public:
-    vector<int>stock;
+    // vector<int>stock;
+    stack<pair<int,int>>st;
+    int ind=-1;
     StockSpanner() {
-        
+        ind=-1;
+        // st.clear();
     }
     
     int next(int price) {
-        int span=0;
-        stock.push_back(price);
-        for(int i=stock.size()-1;i>=0;i--){
-            if(stock[i]<=price) span++;
-            else break;
-        }
+        // int span=0;
+        // stock.push_back(price);
+        // for(int i=stock.size()-1;i>=0;i--){
+        //     if(stock[i]<=price) span++;
+        //     else break;
+        // }
+        // return span;
+        ind++;
+        while(!st.empty() && st.top().first<=price)     st.pop();
+        int span=ind-((st.empty())?-1:st.top().second);
+        st.push({price,ind});
         return span;
     }
 };
