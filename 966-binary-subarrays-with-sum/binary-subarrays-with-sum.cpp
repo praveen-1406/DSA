eced83458d7378942f08211a4ad494b10569de0f
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-06-30 20:14:00 +0530

    Time: 0 ms (100.00%) | Memory: 32.5 MB (80.21%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int sum=0,cnt=0;
+    int func(vector<int>& nums, int goal){
+        if(goal<0)  return 0;
+        int l=0,r=0,sum=0,cnt=0;
         int n=nums.size();
-        unordered_map<int,int>hash;
-        hash[0]=1;
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
-            if(hash.find(sum-goal)!=hash.end()) {
-                cnt+=hash[sum-goal];
+        while(r<n){
+            sum+=nums[r];
+            while(sum>goal){
+                sum-=nums[l];
+                l++;                
             }
-            hash[sum]++;
+            cnt+=(r-l+1);
+            r++;
         }
         return cnt;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        // int sum=0,cnt=0;
+        // int n=nums.size();
+        // unordered_map<int,int>hash;
+        // hash[0]=1;
+        // for(int i=0;i<n;i++){
+        //     sum+=nums[i];
+        //     if(hash.find(sum-goal)!=hash.end()) {
+        //         cnt+=hash[sum-goal];
+        //     }
+        //     hash[sum]++;
+        // }
+        // return cnt;
+        int ans=func(nums,goal)-func(nums,goal-1);
+        return ans;
     }
 };
