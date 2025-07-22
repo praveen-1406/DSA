dc71bee5b810ac8078c8cd4c91b10078689201b2
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-07-22 13:09:13 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (61.35%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -11,23 +11,33 @@
  */
 class Solution {
 public:
+    void reversePreorder(TreeNode*node,int level,vector<int>&ans){
+        if(node==nullptr)   return ;
+        if(level==ans.size())   ans.push_back(node->val);
+        if(node->right)     reversePreorder(node->right,level+1,ans);
+        if(node->left)     reversePreorder(node->left,level+1,ans);
+    }
+
     vector<int> rightSideView(TreeNode* root) {
-        queue<pair<TreeNode*,int>>q;
+        // queue<pair<TreeNode*,int>>q;
+        // vector<int>ans;
+        // if(root==nullptr)   return ans;
+        // map<int,int>mp;
+        // q.push({root,0});
+        // while(!q.empty()){
+        //     TreeNode*node=q.front().first;
+        //     int row=q.front().second;
+        //     q.pop();
+        //     mp[row]=node->val;
+        //     if(node->left)      q.push({node->left,row+1});
+        //     if(node->right)     q.push({node->right,row+1});
+        // }
+        // for(auto it:mp){
+        //     ans.push_back(it.second);
+        // }
+        // return ans;
         vector<int>ans;
-        if(root==nullptr)   return ans;
-        map<int,int>mp;
-        q.push({root,0});
-        while(!q.empty()){
-            TreeNode*node=q.front().first;
-            int row=q.front().second;
-            q.pop();
-            mp[row]=node->val;
-            if(node->left)      q.push({node->left,row+1});
-            if(node->right)     q.push({node->right,row+1});
-        }
-        for(auto it:mp){
-            ans.push_back(it.second);
-        }
+        reversePreorder(root,0,ans);
         return ans;
     }
 };
