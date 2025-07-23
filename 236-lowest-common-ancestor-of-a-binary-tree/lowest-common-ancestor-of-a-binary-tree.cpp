595665fae1fae5ef83df7cdcda98c0bc67f58ce1
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-07-23 14:31:06 +0530

    Time: 15 ms (27.41%) | Memory: 17.6 MB (19.57%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,28 +10,39 @@
 class Solution {
 public:
 
-    bool ancestor(TreeNode* node,TreeNode*p,TreeNode*q,TreeNode*&ans){
-        if(node==nullptr)   return false;
-        bool left=ancestor(node->left,p,q,ans);
-        bool right=ancestor(node->right,p,q,ans);
-        if(left && right){
-            ans=node;
-            return true;
-        }else if(left || right){
-            if(node==p||node==q)    ans=node;
-            return true;
-        }
+    // bool ancestor(TreeNode* node,TreeNode*p,TreeNode*q,TreeNode*&ans){
+    //     if(node==nullptr)   return false;
+    //     bool left=ancestor(node->left,p,q,ans);
+    //     bool right=ancestor(node->right,p,q,ans);
+    //     if(left && right){
+    //         ans=node;
+    //         return true;
+    //     }else if(left || right){
+    //         if(node==p||node==q)    ans=node;
+    //         return true;
+    //     }
 
-        if(node==p || node==q){
-            return true;
-        }
-        return false;
+    //     if(node==p || node==q){
+    //         return true;
+    //     }
+    //     return false;
+    // }
+
+    TreeNode* lca(TreeNode*node,TreeNode*p,TreeNode*q){
+        if(node==nullptr || node==p || node==q)     return node;
+
+        TreeNode* left=lca(node->left,p,q);
+        TreeNode* right=lca(node->right,p,q);
+        if(left==nullptr)   return right;
+        else if(right==nullptr) return left;
+        else    return node;
     }
 
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        TreeNode*ans=nullptr;
-        ancestor(root,p,q,ans);
-        return ans;
+        // TreeNode*ans=nullptr;
+        // ancestor(root,p,q,ans);
+        // return ans;
+        return lca(root,p,q);
     }
 };
 
