6b85f490b73ac4c898e8fddf02d06fcd7c626551
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-07-30 20:02:45 +0530

    Time: 0 ms (100.00%) | Memory: 27.6 MB (37.47%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -13,23 +13,40 @@
 class Solution {
 public:
 
-    TreeNode* uniqueTree(vector<int>& preorder, vector<int>& inorder,int start,int end,int &i){
-        if(start>end)   return nullptr;
-        if(preorder.size()==i)  return nullptr;
-        TreeNode*root=new TreeNode(preorder[i]);
-        int rootIdx=find(inorder.begin(),inorder.end(),root->val)-inorder.begin();
-        i++;
-        TreeNode* left=uniqueTree(preorder,inorder,start,rootIdx-1,i);
-        TreeNode* right=uniqueTree(preorder,inorder,rootIdx+1,end,i);
+    // TreeNode* uniqueTree(vector<int>& preorder, vector<int>& inorder,int start,int end,int &i){
+    //     if(start>end)   return nullptr;
+    //     if(preorder.size()==i)  return nullptr;
+    //     TreeNode*root=new TreeNode(preorder[i]);
+    //     int rootIdx=find(inorder.begin(),inorder.end(),root->val)-inorder.begin();
+    //     i++;
+    //     TreeNode* left=uniqueTree(preorder,inorder,start,rootIdx-1,i);
+    //     TreeNode* right=uniqueTree(preorder,inorder,rootIdx+1,end,i);
 
-        root->left=left;
-        root->right=right;
-        return root;
-    }
+    //     root->left=left;
+    //     root->right=right;
+    //     return root;
+    // }
 
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int i=0;
-        return uniqueTree(preorder,inorder,0,preorder.size()-1,i);
+        // int i=0;
+        // return uniqueTree(preorder,inorder,0,preorder.size()-1,i);
 
+        unordered_map<int,int>mp;
+        for(int i=0;i<inorder.size();i++)   mp[inorder[i]]=i;
+
+        return buildTree(preorder,0,preorder.size()-1,inorder,0,inorder.size()-1,mp);
+
+    }
+    TreeNode* buildTree(vector<int>&preorder,int preStart,int preEnd,vector<int>&inorder,int inStart,int inEnd,unordered_map<int,int>&mp){
+        if(preStart>preEnd || inStart>inEnd)    return nullptr;
+
+        TreeNode*root=new TreeNode(preorder[preStart]);
+        int rootIdx=mp[root->val];
+        int remElem=rootIdx-inStart;
+
+        root->left=buildTree(preorder,preStart+1,preStart+remElem,inorder,inStart,rootIdx-1,mp);
+        root->right=buildTree(preorder,preStart+remElem+1,preEnd,inorder,rootIdx+1,inEnd,mp);
+
+        return root;
     }
 };
