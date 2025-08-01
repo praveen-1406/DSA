e1c27c0c0b9f186484e11f2c09e7ca5eb09f4c0f
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-08-01 14:20:52 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (71.11%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -25,17 +25,43 @@ public:
         // return ans;
 
         // Iterative
-        stack<TreeNode*>st;
-        vector<int>ans;
-        if(root==nullptr)   return ans;
-        st.push(root);
-        while(!st.empty()){           
-            TreeNode*node=st.top();
-            st.pop();
-            ans.push_back(node->val);
-            if(node->right!=nullptr)    st.push(node->right);
-            if(node->left!=nullptr)    st.push(node->left);
+        // stack<TreeNode*>st;
+        // vector<int>ans;
+        // if(root==nullptr)   return ans;
+        // st.push(root);
+        // while(!st.empty()){           
+        //     TreeNode*node=st.top();
+        //     st.pop();
+        //     ans.push_back(node->val);
+        //     if(node->right!=nullptr)    st.push(node->right);
+        //     if(node->left!=nullptr)    st.push(node->left);
+        // }
+        // return ans;
+
+        // Morris Traversal:
+        vector<int>preorder;
+        TreeNode*cur=root;
+        while(cur){
+            if(cur->left==nullptr){
+                preorder.push_back(cur->val);
+                cur=cur->right;
+            }
+            else{
+                TreeNode* prev=cur->left;
+                while(prev->right && prev->right!=cur){
+                    prev=prev->right;
+                }
+                if(prev->right==nullptr){
+                    prev->right=cur;
+                    preorder.push_back(cur->val);
+                    cur=cur->left;
+                }
+                else{
+                    prev->right=nullptr;
+                    cur=cur->right;
+                }
+            }
         }
-        return ans;
+        return preorder;
     }
 };
