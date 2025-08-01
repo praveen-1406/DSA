12a7b314a4004e11b9c0b0bb22105c8cf1f889c3
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-08-01 14:06:16 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (63.38%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -12,6 +12,7 @@
 class Solution {
 public:
 
+    //  Recursive:
     // void Inorder(TreeNode* node,vector<int>&ans){
     //     if(node==nullptr)   return ;
     //     Inorder(node->left,ans);
@@ -24,23 +25,50 @@ public:
         // Inorder(root,ans);
         // return ans;
 
-        // Iterative
-        vector<int>ans;
-        if(root==nullptr)   return ans;
-        stack<TreeNode*>st;
-        TreeNode*node=root;
-        while(true){
-            if(node!=nullptr)   {
-                st.push(node);
-                node=node->left;
-            }else{
-                if(st.empty())  break;
-                node=st.top();
-                st.pop();
-                ans.push_back(node->val);
-                node=node->right;
+        // Iterative:
+        // vector<int>ans;
+        // if(root==nullptr)   return ans;
+        // stack<TreeNode*>st;
+        // TreeNode*node=root;
+        // while(true){
+        //     if(node!=nullptr)   {
+        //         st.push(node);
+        //         node=node->left;
+        //     }else{
+        //         if(st.empty())  break;
+        //         node=st.top();
+        //         st.pop();
+        //         ans.push_back(node->val);
+        //         node=node->right;
+        //     }
+        // }
+        // return ans;
+
+
+        //  Morris Traversal:
+        vector<int>inorder;
+        TreeNode* cur=root;
+        while(cur){
+            if(cur->left==nullptr){
+                inorder.push_back(cur->val);
+                cur=cur->right;
+            }
+            else{
+                TreeNode* prev=cur->left;
+                while(prev->right && prev->right!=cur){
+                    prev=prev->right;
+                }
+                if(prev->right==nullptr){
+                    prev->right=cur;
+                    cur=cur->left;
+                }
+                else{
+                    prev->right=nullptr;
+                    inorder.push_back(cur->val);
+                    cur=cur->right;
+                }
             }
         }
-        return ans;
+        return inorder;
     }
 };
