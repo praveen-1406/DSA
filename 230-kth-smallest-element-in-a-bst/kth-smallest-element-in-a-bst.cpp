36854a4343c431939e1f49f6eb260488d682521e
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-08-04 15:27:16 +0530

    Time: 0 ms (100.00%) | Memory: 24.4 MB (66.00%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -11,20 +11,44 @@
  */
 class Solution {
 public:
-    void preorder(TreeNode*root,set<int>&st){
-        if(root==nullptr)   return;
-        st.insert(root->val);
-        preorder(root->left,st);
-        preorder(root->right,st);
-    }
+    // void preorder(TreeNode*root,set<int>&st){
+    //     if(root==nullptr)   return;
+    //     st.insert(root->val);
+    //     preorder(root->left,st);
+    //     preorder(root->right,st);
+    // }
     int kthSmallest(TreeNode* root, int k) {
-        set<int>st;
-        preorder(root,st);
+        // set<int>st;
+        // preorder(root,st);
 
-        for(auto it:st){
-            if(k==1)    return it;
-            k--;
+        // for(auto it:st){
+        //     if(k==1)    return it;
+        //     k--;
+        // }
+        // return -1;
+
+        //Morris inorder traversal.
+        TreeNode*node=root;
+        int cnt=0,result=-1;
+        while(node){
+            if(node->left==nullptr){
+                cnt++;
+                if(cnt==k)  result= node->val;
+                node=node->right;
+            }else{
+                TreeNode*rightMost=node->left;
+                while(rightMost->right && rightMost->right!=node)  rightMost=rightMost->right;
+                if(rightMost->right==node){
+                    cnt++;
+                    if(cnt==k)  result= node->val;
+                    rightMost->right=nullptr;
+                    node=node->right;
+                }else{
+                    rightMost->right=node;
+                    node=node->left;
+                }
+            }
         }
-        return -1;
+        return result;
     }
 };
