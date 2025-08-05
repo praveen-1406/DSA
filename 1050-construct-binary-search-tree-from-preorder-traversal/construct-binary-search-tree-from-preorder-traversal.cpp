c0b3e3159f82cd2d4557a624db88af49e74acfc8
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-08-05 17:52:20 +0530

    Time: 4 ms (9.13%) | Memory: 16.5 MB (23.52%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,24 +11,33 @@
  */
 class Solution {
 public:
-    void createBST(TreeNode*node,vector<int>&preorder,int &idx,int end){
-        if(idx==preorder.size())    return ;
-        TreeNode*cur=new TreeNode(preorder[idx]);
-        if(cur->val < node->val) {
-            node->left=cur;
-            createBST(cur,preorder,++idx,node->val);
-        }
-        if(idx<preorder.size())    cur=new TreeNode(preorder[idx]);
-        if(cur->val > node->val && cur->val < end){
-            node->right=cur;
-            createBST(cur,preorder,++idx,end);
-        }
-        // else idx--;
+    // void createBST(TreeNode*node,vector<int>&preorder,int &idx,int end){
+    //     if(idx==preorder.size())    return ;
+    //     TreeNode*cur=new TreeNode(preorder[idx]);
+    //     if(cur->val < node->val) {
+    //         node->left=cur;
+    //         createBST(cur,preorder,++idx,node->val);
+    //     }
+    //     if(idx<preorder.size())    cur=new TreeNode(preorder[idx]);
+    //     if(cur->val > node->val && cur->val < end){
+    //         node->right=cur;
+    //         createBST(cur,preorder,++idx,end);
+    //     }
+    //     // else idx--;
+    // }
+    TreeNode*build(vector<int>& preorder,int &i,int bound){
+        if(i==preorder.size()||preorder[i]>bound)   return nullptr;
+        TreeNode*root=new TreeNode(preorder[i++]);
+        root->left=build(preorder,i,root->val);
+        root->right=build(preorder,i,bound);
+        return root;
     }
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        TreeNode*root=new TreeNode(preorder[0]);
-        int idx=1;
-        createBST(root,preorder,idx,1001);
-        return root;
+        // TreeNode*root=new TreeNode(preorder[0]);
+        // int idx=1;
+        // createBST(root,preorder,idx,1001);
+        // return root;
+        int i=0;
+        return build(preorder,i,INT_MAX);
     }
 };
