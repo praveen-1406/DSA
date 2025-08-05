199c62e64ac231067f64b45363119fa2739d0108
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-08-05 17:26:21 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (89.98%) - LeetSync

---
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void createBST(TreeNode*node,vector<int>&preorder,int &idx,int start,int end){
+        if(idx==preorder.size())    return ;
+        TreeNode*cur=new TreeNode(preorder[idx]);
+        if(cur->val > start && cur->val < node->val) {
+            node->left=cur;
+            createBST(cur,preorder,++idx,start,node->val);
+        }
+        if(idx<preorder.size())    cur=new TreeNode(preorder[idx]);
+        if(cur->val > node->val && cur->val < end){
+            node->right=cur;
+            createBST(cur,preorder,++idx,node->val,end);
+        }
+        // else idx--;
+    }
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        TreeNode*root=new TreeNode(preorder[0]);
+        int idx=1;
+        createBST(root,preorder,idx,0,1001);
+        return root;
+    }
+};
