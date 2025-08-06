8c687c54b14bf3b35c83ca66bc0916f18af3fe83
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-08-06 12:16:41 +0530

    Time: 0 ms (100.00%) | Memory: 32 MB (67.60%) - LeetSync

--- 173-binary-search-tree-iterator/binary-search-tree-iterator.cpp
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.cpp
@@ -11,44 +11,59 @@
  */
 class BSTIterator {
 public:
-    vector<int>inorder;
-    TreeNode*root=nullptr;
-    int idx=-1;
+    // vector<int>inorder;
+    // TreeNode*root=nullptr;
+    // int idx=-1;
+    stack<TreeNode*>st;
     BSTIterator(TreeNode* root) {
-        this->root=root;
-        MorrisInorder(root);
-        idx++;
-    }
-    
-    void MorrisInorder(TreeNode*root){
+        // this->root=root;
+        // MorrisInorder(root);
+        // idx++;
         TreeNode*node=root;
         while(node){
-            if(node->left==nullptr){
-                inorder.push_back(node->val);
-                node=node->right;
-            }
-            else{
-                TreeNode*pre=node->left;
-                while(pre->right && pre->right!=node)   pre=pre->right;
-                if(pre->right==node){
-                    inorder.push_back(node->val);
-                    pre->right=nullptr;
-                    node=node->right;
-                }else{
-                    pre->right=node;
-                    node=node->left;
-                }  
-            }
+            st.push(node);
+            node=node->left;
         }
     }
+    
+    // void MorrisInorder(TreeNode*root){
+    //     TreeNode*node=root;
+    //     while(node){
+    //         if(node->left==nullptr){
+    //             inorder.push_back(node->val);
+    //             node=node->right;
+    //         }
+    //         else{
+    //             TreeNode*pre=node->left;
+    //             while(pre->right && pre->right!=node)   pre=pre->right;
+    //             if(pre->right==node){
+    //                 inorder.push_back(node->val);
+    //                 pre->right=nullptr;
+    //                 node=node->right;
+    //             }else{
+    //                 pre->right=node;
+    //                 node=node->left;
+    //             }  
+    //         }
+    //     }
+    // }
 
     int next() {
-        return inorder[idx++];
+        // return inorder[idx++];
+        int ans=st.top()->val;
+        TreeNode*node=st.top()->right;
+        st.pop();
+        while(node){
+            st.push(node);
+            node=node->left;
+        }
+        return ans;
     }
     
     bool hasNext() {
-        if(idx<inorder.size())  return true;
-        else    return false;
+        // if(idx<inorder.size())  return true;
+        // else    return false;
+        return !st.empty();
     }
 };
 
