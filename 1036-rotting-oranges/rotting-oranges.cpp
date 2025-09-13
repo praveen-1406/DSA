851c9918b53b49aec48c3b087ff6ff17b48a37a1
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-09-13 14:27:59 +0530

    Time: 4 ms (15.99%) | Memory: 17.1 MB (46.34%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -17,27 +17,38 @@ public:
             }
         }
         
+        int drow[] ={-1,0,+1,0};
+        int dcol[] ={0,-1,0,+1};
+
         while(!q.empty()){
             int row=q.front().first.first;
             int col=q.front().first.second;
             int time=q.front().second;
             q.pop();
             maxtime=max(maxtime,time);
-            if(row>0 && !vis[row-1][col] && grid[row-1][col]==1){
-                q.push({{row-1,col},time+1});
-                vis[row-1][col]=2;
-            }
-            if(row<n-1 && !vis[row+1][col] && grid[row+1][col]==1){
-                q.push({{row+1,col},time+1});
-                vis[row+1][col]=2;
-            }
-            if(col>0 && !vis[row][col-1] && grid[row][col-1]==1){
-                q.push({{row,col-1},time+1});
-                vis[row][col-1]=2;
-            }
-            if(col<m-1 && !vis[row][col+1] && grid[row][col+1]==1){
-                q.push({{row,col+1},time+1});
-                vis[row][col+1]=2;
+            // if(row>0 && !vis[row-1][col] && grid[row-1][col]==1){
+            //     q.push({{row-1,col},time+1});
+            //     vis[row-1][col]=2;
+            // }
+            // if(row<n-1 && !vis[row+1][col] && grid[row+1][col]==1){
+            //     q.push({{row+1,col},time+1});
+            //     vis[row+1][col]=2;
+            // }
+            // if(col>0 && !vis[row][col-1] && grid[row][col-1]==1){
+            //     q.push({{row,col-1},time+1});
+            //     vis[row][col-1]=2;
+            // }
+            // if(col<m-1 && !vis[row][col+1] && grid[row][col+1]==1){
+            //     q.push({{row,col+1},time+1});
+            //     vis[row][col+1]=2;
+            // }
+            for(int i=0;i<4;i++){
+                int nrow=row+drow[i];
+                int ncol=col+dcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && grid[nrow][ncol]==1){
+                    vis[nrow][ncol]=1;
+                    q.push({{nrow,ncol},time+1});
+                }
             }
         }
 
