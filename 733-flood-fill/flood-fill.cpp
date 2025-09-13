4efc032bad74c43760dff7765c35c2de653182b1
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-09-14 01:50:24 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (47.34%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,4 +1,18 @@
 class Solution {
+private:
+    void dfs(int row,int col,vector<vector<int>>& image,vector<vector<int>>&ans,int drow[],int dcol[],int init,int color){
+        ans[row][col]=color;
+        int n=image.size();
+        int m=image[0].size();
+        for(int i=0;i<4;i++){
+            int nrow=row+drow[i];
+            int ncol=col+dcol[i];
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && ans[nrow][ncol]==init){
+                dfs(nrow,ncol,image,ans,drow,dcol,init,color);
+            }
+        }
+    }
+
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n=image.size();
@@ -8,23 +22,24 @@ public:
         vector<vector<int>>ans=image;
         q.push({sr,sc});
         if(init == color) return image;
-        ans[sr][sc]=color;
+        // ans[sr][sc]=color;
         int drow[]={-1,0,+1,0};
         int dcol[]={0,-1,0,+1};
 
-        while(!q.empty()){
-            int row=q.front().first;
-            int col=q.front().second;
-            q.pop();
-            for(int i=0;i<4;i++){
-                int nrow=row+drow[i];
-                int ncol=col+dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && ans[nrow][ncol]==init){
-                        ans[nrow][ncol]=color;
-                        q.push({nrow,ncol});
-                }
-            }
-        }
+        // while(!q.empty()){
+        //     int row=q.front().first;
+        //     int col=q.front().second;
+        //     q.pop();
+        //     for(int i=0;i<4;i++){
+        //         int nrow=row+drow[i];
+        //         int ncol=col+dcol[i];
+        //         if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && ans[nrow][ncol]==init){
+        //                 ans[nrow][ncol]=color;
+        //                 q.push({nrow,ncol});
+        //         }
+        //     }
+        // }
+        dfs(sr,sc,image,ans,drow,dcol,init,color);
         return ans;
     }
 };
