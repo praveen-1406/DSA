957423e5fd2760a42af5262e30ca441439efdf47
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-09-17 12:40:30 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (48.71%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -24,22 +24,19 @@ public:
             if(board[i][0]=='O' && !vis[i][0]){
                 dfs(i,0,board,vis);
             }
-        }
-        for(int j=0;j<m;j++){
-            if(board[0][j]=='O' && !vis[0][j]){
-                dfs(0,j,board,vis);
-            }
-        }
-        for(int i=0;i<n;i++){
             if(board[i][m-1]=='O' && !vis[i][m-1]){
                 dfs(i,m-1,board,vis);
             }
         }
         for(int j=0;j<m;j++){
+            if(board[0][j]=='O' && !vis[0][j]){
+                dfs(0,j,board,vis);
+            }
             if(board[n-1][j]=='O' && !vis[n-1][j]){
                 dfs(n-1,j,board,vis);
             }
         }
+        
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
