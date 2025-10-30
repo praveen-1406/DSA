a3b4fee94764e631cd0cb503994e57d4c1ea5328
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-10-30 14:30:13 +0530

    Time: 25 ms (34.28%) | Memory: 46.6 MB (61.91%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,6 +1,6 @@
 class DisjointSet{
-    vector<int>size,parent;
 public:
+    vector<int>size,parent;
     DisjointSet(int n){
         size.resize(n,1);
         parent.resize(n);
@@ -29,16 +29,34 @@ public:
     int makeConnected(int n, vector<vector<int>>& connections) {
         if(connections.size()<n-1)   return -1;
         DisjointSet ds(n);
+        // for(auto it:connections){
+        //     ds.unionBySize(it[0],it[1]);
+        // }
+        // int steps=0;
+        // for(int i=1;i<n;i++){
+        //     if(ds.findParent(0)!=ds.findParent(i)){
+        //         steps++;
+        //         ds.unionBySize(0,i);
+        //     }
+        // }
+        // return steps;
+
+        int extra=0;
         for(auto it:connections){
-            ds.unionBySize(it[0],it[1]);
-        }
-        int steps=0;
-        for(int i=1;i<n;i++){
-            if(ds.findParent(0)!=ds.findParent(i)){
-                steps++;
-                ds.unionBySize(0,i);
+            int u=it[0],v=it[1];
+            if(ds.findParent(u)==ds.findParent(v)){
+                extra++;
+            }else{
+                ds.unionBySize(u,v);
             }
         }
-        return steps;
+        int comp=0;
+        for(int i=0;i<n;i++){
+            if(ds.parent[i]==i)     comp++;
+        }
+        int ans=comp-1;
+        if(extra>=ans)  return ans;
+        return -1;
+
     }
 };
