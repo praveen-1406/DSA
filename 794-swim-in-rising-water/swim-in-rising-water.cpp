920fd926a9510b64746e7bc6d04c5868cf25cb9d
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-11-04 22:48:12 +0530

    Time: 12 ms (33.25%) | Memory: 15.2 MB (29.92%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -16,21 +16,44 @@ class Solution {
     }
 public:
     int swimInWater(vector<vector<int>>& grid) {
-        // priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        // int n=grid.size();
+        // int l=grid[0][0];
+        // int r=n*n-1;
+        // int result=0;
+        // while(l<=r){
+        //     int mid=l+(r-l)/2;
+        //     vector<vector<bool>>visited(n,vector<bool>(n,false));
+        //     if(possible(0,0,mid,visited,grid)){
+        //         result=mid;
+        //         r=mid-1;
+        //     }else{
+        //         l=mid+1;
+        //     }
+        // }
+        // return result;
+
+        // Dijkstra's Algo
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
         int n=grid.size();
-        int l=grid[0][0];
-        int r=n*n-1;
-        int result=0;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            vector<vector<bool>>visited(n,vector<bool>(n,false));
-            if(possible(0,0,mid,visited,grid)){
-                result=mid;
-                r=mid-1;
-            }else{
-                l=mid+1;
+        vector<vector<int>>visited(n,vector<int>(n,0));
+        pq.push({grid[0][0],0,0});
+        visited[0][0]=1;
+        int ans=0;
+        while(!pq.empty()){
+            auto it=pq.top();
+            pq.pop();
+            int h=it[0],x=it[1],y=it[2];
+            ans=max(ans,h);
+            if(x==n-1 && y==n-1)    return ans;
+            for(auto &d:dir){
+                int nx=x+d[0];
+                int ny=y+d[1];
+                if(nx>=0 && ny>=0 && nx<n && ny<n && !visited[nx][ny]){
+                    visited[nx][ny]=1;
+                    pq.push({grid[nx][ny],nx,ny});
+                }
             }
         }
-        return result;
+        return ans;
     }
 };
