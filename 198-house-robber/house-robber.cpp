e19031a609e7b0c1e82abf98ba3936f4e1328910
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-11-11 18:19:47 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (22.40%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,23 +1,26 @@
 class Solution {
-    // int f(int ind,vector<int>&nums){
-    //     if(ind<0)  return 0;
+    // Memoization
+    // int f(int ind,vector<int>&nums,vector<int>&dp){
+    //     if(ind<0)   return 0;
+    //     if(dp[ind]!=-1) return dp[ind];
     //     int m1=0,m2=0;
-    //     if(ind-2>=0)    m1=f(ind-2,nums);
-    //     if(ind-3>=0)    m2=f(ind-3,nums);
-    //     return max(nums[ind]+m1,nums[ind]+m2);
+    //     m1=nums[ind]+f(ind-2,nums,dp);
+    //     m2=f(ind-1,nums,dp);
+    //     return dp[ind]=max(m1,m2);
     // }
-    int f(int ind,vector<int>&nums,vector<int>&dp){
-        if(ind<0)   return 0;
-        if(dp[ind]!=-1) return dp[ind];
-        int m1=0,m2=0;
-        m1=nums[ind]+f(ind-2,nums,dp);
-        m2=f(ind-1,nums,dp);
-        return dp[ind]=max(m1,m2);
-    }
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n,-1);
-        return f(n-1,nums,dp);
+        vector<int>dp(n);
+        // return f(n-1,nums,dp);
+
+        // Tabulation
+        if(n==1)    return nums[0];
+        dp[0]=nums[0],dp[1]=max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[n-1];
+
     }   
 };
