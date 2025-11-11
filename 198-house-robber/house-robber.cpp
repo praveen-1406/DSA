a8eb5671611779190520bdc8d20978afc194a000
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-11-11 18:23:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (22.40%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -15,12 +15,22 @@ public:
         // return f(n-1,nums,dp);
 
         // Tabulation
+        // if(n==1)    return nums[0];
+        // dp[0]=nums[0],dp[1]=max(nums[0],nums[1]);
+        // for(int i=2;i<n;i++){
+        //     dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+        // }
+        // return dp[n-1];
+
+        // Space optimization
         if(n==1)    return nums[0];
-        dp[0]=nums[0],dp[1]=max(nums[0],nums[1]);
+        int prev2=nums[0],prev=max(nums[0],nums[1]),curi;
         for(int i=2;i<n;i++){
-            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+            curi=max(nums[i]+prev2,prev);
+            prev2=prev;
+            prev=curi;
         }
-        return dp[n-1];
+        return prev;
 
     }   
 };
