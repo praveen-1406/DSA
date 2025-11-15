e4d21078e0ec1510e9b27edde7ba1637a2c2838e
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-11-15 20:19:18 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (74.41%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -30,20 +30,37 @@ public:
         // return f(m-1,n-1,grid,dp);
 
         // Tabulation
-        vector<vector<int>>dp(m,vector<int>(n,0));
-        
+        // vector<vector<int>>dp(m,vector<int>(n,0));
+        // for(int row=0;row<m;row++){
+        //     for(int col=0;col<n;col++){
+        //         if(row==0 && col==0)    dp[0][0]=grid[0][0];
+        //         else{
+        //             int up=INT_MAX,left=INT_MAX;
+        //             if(row>0)   up=dp[row-1][col];
+        //             if(col>0)   left=dp[row][col-1];
+        //             dp[row][col]=grid[row][col]+min(up,left);
+        //         }
+        //     }
+        // }
+        // return dp[m-1][n-1];
+
+        // Space Optimization
+        vector<int>preRow(n,INT_MAX);
         for(int row=0;row<m;row++){
+            vector<int>temp(n,0);
             for(int col=0;col<n;col++){
-                if(row==0 && col==0)    dp[0][0]=grid[0][0];
+                if(row==0 && col==0)    temp[col]=grid[0][0];
                 else{
                     int up=INT_MAX,left=INT_MAX;
-                    if(row>0)   up=dp[row-1][col];
-                    if(col>0)   left=dp[row][col-1];
-                    dp[row][col]=grid[row][col]+min(up,left);
+                    up=preRow[col];
+                    if(col>0)   left=temp[col-1];
+                    temp[col]=grid[row][col]+min(up,left);
                 }
             }
+            preRow=temp;
         }
-        return dp[m-1][n-1];
+        return preRow[n-1];
+
     }
 };
 
