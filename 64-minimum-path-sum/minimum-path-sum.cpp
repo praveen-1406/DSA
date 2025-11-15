0eec49d853dce08c27b2a216c1c60d7acbf14637
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-11-15 20:05:19 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (58.19%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -9,14 +9,14 @@ class Solution {
     // }
 
     // Memoization
-    int f(int row,int col,vector<vector<int>>&grid,vector<vector<int>>&dp){
-        if(row==0 && col==0)  return grid[row][col];
-        if(dp[row][col]!=-1)    return dp[row][col];
-        int up=INT_MAX,left=INT_MAX;
-        if(row>0) up=f(row-1,col,grid,dp);
-        if(col>0) left=f(row,col-1,grid,dp);
-        return dp[row][col]=grid[row][col]+min(up,left);   
-    }
+    // int f(int row,int col,vector<vector<int>>&grid,vector<vector<int>>&dp){
+    //     if(row==0 && col==0)  return grid[row][col];
+    //     if(dp[row][col]!=-1)    return dp[row][col];
+    //     int up=INT_MAX,left=INT_MAX;
+    //     if(row>0) up=f(row-1,col,grid,dp);
+    //     if(col>0) left=f(row,col-1,grid,dp);
+    //     return dp[row][col]=grid[row][col]+min(up,left);   
+    // }
 
 public:
     int minPathSum(vector<vector<int>>& grid) {
@@ -26,9 +26,24 @@ public:
         // return f(m-1,n-1,grid);
 
         // Memoization
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return f(m-1,n-1,grid,dp);
+        // vector<vector<int>>dp(m,vector<int>(n,-1));
+        // return f(m-1,n-1,grid,dp);
 
+        // Tabulation
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        
+        for(int row=0;row<m;row++){
+            for(int col=0;col<n;col++){
+                if(row==0 && col==0)    dp[0][0]=grid[0][0];
+                else{
+                    int up=INT_MAX,left=INT_MAX;
+                    if(row>0)   up=dp[row-1][col];
+                    if(col>0)   left=dp[row][col-1];
+                    dp[row][col]=grid[row][col]+min(up,left);
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
 
