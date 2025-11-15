50015cdeceaae8ce153760eda3c4d16e75b8ca46
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-11-15 16:55:58 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (52.78%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -28,8 +28,24 @@ public:
         // return f(m-1,n-1,grid);
 
         // Memoization
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return f(m-1,n-1,grid,dp);
+        // vector<vector<int>>dp(m,vector<int>(n,-1));
+        // return f(m-1,n-1,grid,dp);
+
+        // Tabulation
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        dp[0][0]=1;
+        for(int row=0;row<m;row++){
+            for(int col=0;col<n;col++){
+                if(grid[row][col]==1)   {
+                    dp[row][col]=0;
+                    continue;
+                }
+                if(row==0 && col==0)    continue;
+                if(col>0 && grid[row][col-1]!=1)   dp[row][col]+=dp[row][col-1];
+                if(row>0 && grid[row-1][col]!=1)   dp[row][col]+=dp[row-1][col];
+            }
+        }
+        return dp[m-1][n-1];
 
     }
 };
