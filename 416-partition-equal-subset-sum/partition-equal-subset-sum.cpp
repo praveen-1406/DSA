5a8ed1f2f442db6c2641a2a5967b48eebbdc41d5
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2025-11-20 22:51:05 +0530

    Time: 143 ms (40.90%) | Memory: 17.5 MB (59.53%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -40,7 +40,7 @@ public:
         // Tabulation
         vector<vector<bool>>dp(n,vector<bool>(k+1,false));
         for(int i=0;i<n;i++)    dp[i][0]=true;
-        if(nums[0]<=k)    dp[0][nums[0]]=true;
+        if(nums[0]==k)    dp[0][nums[0]]=true;
         for(int ind=1;ind<n;ind++){
             for(int tar=1;tar<=k;tar++){
                 bool notPick=dp[ind-1][tar];
