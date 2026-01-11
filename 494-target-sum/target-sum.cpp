6f05e9a43bd823acad8af6e02d71d048639f5bf1
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2026-01-11 23:00:21 +0530

    Time: 3 ms (85.77%) | Memory: 15.4 MB (49.07%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -14,19 +14,19 @@ class Solution {
 
     // }
 
-    int f(int ind,int k,vector<int>&nums,vector<vector<int>>&dp){
-        if(ind==0){
-            if(k==0 && nums[0]==0) return 2;
-            if(k==0) return 1;
-            return (k==nums[0]);
-            return 0;
-        }
-        if(dp[ind][k]!=-1)  return dp[ind][k];
-        int pick=0;
-        if(k>=nums[ind])    pick=f(ind-1,k-nums[ind],nums,dp);
-        int notPick=f(ind-1,k,nums,dp);
-        return dp[ind][k]=(pick+notPick);
-    }
+    // int f(int ind,int k,vector<int>&nums,vector<vector<int>>&dp){
+    //     if(ind==0){
+    //         if(k==0 && nums[0]==0) return 2;
+    //         if(k==0) return 1;
+    //         return (k==nums[0]);
+    //         return 0;
+    //     }
+    //     if(dp[ind][k]!=-1)  return dp[ind][k];
+    //     int pick=0;
+    //     if(k>=nums[ind])    pick=f(ind-1,k-nums[ind],nums,dp);
+    //     int notPick=f(ind-1,k,nums,dp);
+    //     return dp[ind][k]=(pick+notPick);
+    // }
 
 
 public:
@@ -41,8 +41,23 @@ public:
         if((target>sum)||(sum-target)%2)    return 0;
         int k=(sum-target)/2;
 
-        vector<vector<int>>dp(n,vector<int>(k+1,-1));
-        return f(n-1,k,nums,dp);
+        // vector<vector<int>>dp(n,vector<int>(k+1,-1));
+        // return f(n-1,k,nums,dp);
+
+        vector<vector<int>>dp(n,vector<int>(k+1,0));
+        dp[0][0]=1;
+        if(nums[0]==0)  dp[0][0]=2;
+        if(nums[0]!=0 && k>=nums[0])  dp[0][nums[0]]=1;
+
+        for(int ind=1;ind<n;ind++){
+            for(int tar=0;tar<=k;tar++){
+                int pick=0;
+                if(tar>=nums[ind])    pick=dp[ind-1][tar-nums[ind]];
+                int notPick=dp[ind-1][tar];
+                dp[ind][tar]=(pick+notPick);
+            }
+        }
+        return dp[n-1][k];
     }
 };
 
