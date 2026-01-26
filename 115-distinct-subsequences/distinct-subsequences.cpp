8b8518cd969b909b32c28d66badaba1e1df81cd9
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2026-01-27 03:05:01 +0530

    Time: 31 ms (51.88%) | Memory: 26.9 MB (69.66%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,9 +1,9 @@
 class Solution {
     int f(int i,int j,string &s,string &t,int len,vector<vector<int>>&dp){
-        if(i==0 || j==0){
-            if(j==0)   return 1;
-            return 0;
-        }
+        
+            if(j==0)    return 1;
+            if(i==0)    return 0;
+        
         if(dp[i][j]!=-1)    return dp[i][j];
         int ans=0;
         if(s[i-1]==t[j-1])  ans+= f(i-1,j-1,s,t,len+1,dp);
