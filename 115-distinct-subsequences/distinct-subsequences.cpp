7b370a336b924337fad4ebbbaede23898a9a539c
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2026-01-27 03:26:36 +0530

    Time: 15 ms (81.42%) | Memory: 8.8 MB (98.30%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -18,16 +18,20 @@ public:
         // vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
         // return f(n,m,s,t,0,dp);
 
-        vector<vector<unsigned long long>>dp(n+1,vector<unsigned long long>(m+1,0));
-        for(int j=0;j<=m;j++)   dp[0][j]=0;         //where i is 0
-        for(int i=0;i<=n;i++)    dp[i][0]=1;         //where j is 0
+        // vector<vector<unsigned long long>>dp(n+1,vector<unsigned long long>(m+1,0));
+        vector<unsigned long long>pre(m+1,0),cur(m+1,0);
+        for(int j=0;j<=m;j++)   pre[j]=0;         //where i is 0
+        // for(int i=0;i<=n;i++)    dp[i][0]=1;         //where j is 0
+        pre[0]=1;
         for(int i=1;i<=n;i++){
+            cur[0]=1;
             for(int j=1;j<=m;j++){
-                if(s[i-1]==t[j-1])  dp[i][j]+=dp[i-1][j-1];
-                dp[i][j]+=dp[i-1][j];
+                cur[j]=pre[j];
+                if(s[i-1]==t[j-1])  cur[j]+=pre[j-1];
             }
+            pre=cur;
         }
-        return (int)dp[n][m];
+        return (int)pre[m];
     }
 };
 
