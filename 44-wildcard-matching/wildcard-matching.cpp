664cf9d1d73456334bf0120293122399c5dc0088
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2026-02-10 00:02:12 +0530

    Time: 43 ms (66.33%) | Memory: 10.2 MB (84.48%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -24,24 +24,45 @@ public:
         // vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
         // return f(n,m,s,p,dp);
 
-        vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
-        dp[0][0]=true;
-        for(int i=1;i<=n;i++)   dp[i][0]=false;
+        // vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
+        // dp[0][0]=true;
+        // for(int i=1;i<=n;i++)   dp[i][0]=false;
+        // for(int j=1;j<=m;j++){
+        //     if(p[j-1]=='*') dp[0][j]=true;
+        //     else break;
+        // }
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=m;j++){
+        //         if(p[j-1]==s[i-1] || p[j-1]=='?')  {
+        //             dp[i][j]= dp[i-1][j-1];
+        //         }
+        //         else if(p[j-1]=='*'){
+        //             dp[i][j]= dp[i-1][j] || dp[i][j-1];
+        //         }
+        //         else dp[i][j]=false;
+        //     }
+        // }
+        // return dp[n][m];
+
+        vector<bool>pre(m+1,false),cur(m+1,false);
+        pre[0]=true;
         for(int j=1;j<=m;j++){
-            if(p[j-1]=='*') dp[0][j]=true;
+            if(p[j-1]=='*') pre[j]=true;
             else break;
         }
         for(int i=1;i<=n;i++){
+            cur[0]=false;
             for(int j=1;j<=m;j++){
                 if(p[j-1]==s[i-1] || p[j-1]=='?')  {
-                    dp[i][j]= dp[i-1][j-1];
+                    cur[j]= pre[j-1];
                 }
                 else if(p[j-1]=='*'){
-                    dp[i][j]= dp[i-1][j] || dp[i][j-1];
+                    cur[j]= pre[j] || cur[j-1];
                 }
-                else dp[i][j]=false;
+                else cur[j]=false;
             }
+            pre=cur;
         }
-        return dp[n][m];
+        return pre[m];
     }
 };
