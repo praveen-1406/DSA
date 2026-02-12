48e02dcc413b915ee66b2983cb1087c3d13de7be
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2026-02-12 13:09:28 +0530

    Time: 11 ms (44.21%) | Memory: 17.8 MB (55.27%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,16 +1,29 @@
 class Solution {
-    int f(int ind,int buy,int cap,vector<int>& prices,vector<vector<vector<int>>>&dp){
-        if(cap==0)  return 0;
-        if(ind==prices.size())  return 0;
-        if(dp[ind][buy][cap]!=-1)   return dp[ind][buy][cap];
+    // int f(int ind,int buy,int cap,vector<int>& prices,vector<vector<vector<int>>>&dp){
+    //     if(cap==0)  return 0;
+    //     if(ind==prices.size())  return 0;
+    //     if(dp[ind][buy][cap]!=-1)   return dp[ind][buy][cap];
+    //     int profit;
+    //     if(buy){
+    //         profit=max(-prices[ind]+f(ind+1,0,cap,prices,dp),0+f(ind+1,1,cap,prices,dp));
+    //     }
+    //     else{
+    //         profit=max(prices[ind]+f(ind+1,1,cap-1,prices,dp),0+f(ind+1,0,cap,prices,dp));
+    //     }
+    //     return dp[ind][buy][cap]=profit;
+    // }
+
+    int f(int ind,int tran,int k,vector<int>& prices,vector<vector<int>>&dp){
+        if(ind==prices.size() || tran==2*k)  return 0;
+        if(dp[ind][tran]!=-1)   return dp[ind][tran];
         int profit;
-        if(buy){
-            profit=max(-prices[ind]+f(ind+1,0,cap,prices,dp),0+f(ind+1,1,cap,prices,dp));
+        if(tran%2==0){  //buy
+            profit=max(-prices[ind]+f(ind+1,tran+1,k,prices,dp),0+f(ind+1,tran,k,prices,dp));
         }
-        else{
-            profit=max(prices[ind]+f(ind+1,1,cap-1,prices,dp),0+f(ind+1,0,cap,prices,dp));
+        else{   //sell
+            profit=max(prices[ind]+f(ind+1,tran+1,k,prices,dp),0+f(ind+1,tran,k,prices,dp));
         }
-        return dp[ind][buy][cap]=profit;
+        return dp[ind][tran]=profit;
     }
 public:
     int maxProfit(int k, vector<int>& prices) {
@@ -35,22 +48,25 @@ public:
         // }
         // return dp[0][1][k];
 
-        vector<vector<int>>after(2,vector<int>(k+1,0)),cur(2,vector<int>(k+1,0));
-        for(int ind=n-1;ind>=0;ind--){
-            for(int buy=0;buy<2;buy++){
-                for(int cap=1;cap<=k;cap++){
-                    int profit;
-                    if(buy){
-                        profit=max(-prices[ind]+after[0][cap],0+after[1][cap]);
-                    }
-                    else{
-                        profit=max(prices[ind]+after[1][cap-1],0+after[0][cap]);
-                    }
-                    cur[buy][cap]=profit;
-                }
-                after=cur;
-            }
-        }
-        return after[1][k];
+        // vector<vector<int>>after(2,vector<int>(k+1,0)),cur(2,vector<int>(k+1,0));
+        // for(int ind=n-1;ind>=0;ind--){
+        //     for(int buy=0;buy<2;buy++){
+        //         for(int cap=1;cap<=k;cap++){
+        //             int profit;
+        //             if(buy){
+        //                 profit=max(-prices[ind]+after[0][cap],0+after[1][cap]);
+        //             }
+        //             else{
+        //                 profit=max(prices[ind]+after[1][cap-1],0+after[0][cap]);
+        //             }
+        //             cur[buy][cap]=profit;
+        //         }
+        //         after=cur;
+        //     }
+        // }
+        // return after[1][k];
+
+        vector<vector<int>>dp(n,vector<int>(2*k,-1));
+        return f(0,0,k,prices,dp);
     }
 };
