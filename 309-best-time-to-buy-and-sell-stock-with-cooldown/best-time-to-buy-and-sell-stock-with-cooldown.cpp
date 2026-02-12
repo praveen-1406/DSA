43ab2aa099a647048efd4b10c36f1898af781d32
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2026-02-12 17:44:50 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (77.96%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -13,16 +13,30 @@ public:
         // vector<vector<int>>dp(n,vector<int>(2,-1));
         // return f(0,1,prices,dp);
 
-        vector<vector<int>>dp(n+2,vector<int>(2,0));
+        // vector<vector<int>>dp(n+2,vector<int>(2,0));
+        // for(int ind=n-1;ind>=0;ind--){
+        //     for(int buy=0;buy<2;buy++){
+        //         int profit;
+        //         if(buy)     profit=max(-prices[ind]+dp[ind+1][0],0+dp[ind+1][1]);
+        //         else    profit=max(prices[ind]+dp[ind+2][1],0+dp[ind+1][0]);
+        //         dp[ind][buy]=profit;
+        //     }
+        // }
+        // return dp[0][1];
+
+        vector<int>cur(2,0),after(2,0);
+        int preafter=0;
         for(int ind=n-1;ind>=0;ind--){
             for(int buy=0;buy<2;buy++){
                 int profit;
-                if(buy)     profit=max(-prices[ind]+dp[ind+1][0],0+dp[ind+1][1]);
-                else    profit=max(prices[ind]+dp[ind+2][1],0+dp[ind+1][0]);
-                dp[ind][buy]=profit;
+                if(buy)     profit=max(-prices[ind]+after[0],0+after[1]);
+                else    profit=max(prices[ind]+preafter,0+after[0]);
+                cur[buy]=profit;
             }
+            preafter=after[1];
+            after=cur;
         }
-        return dp[0][1];
+        return after[1];
 
     }
 };
