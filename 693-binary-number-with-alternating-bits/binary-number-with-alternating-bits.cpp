39f7d6939dab25e4ae89bf48e64c22f995f10888
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2026-02-18 12:38:58 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (91.43%) - LeetSync

--- 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cpp
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
     bool hasAlternatingBits(int n) {
-        int i=31;
-        for(i;i>=0;i--){
-            int bit=(((1<<i)&n)==0)?0:1;
-            if(bit==1)    break;
-        }
-        bool isOne=1;
-        for(i;i>=0;i--){
-            int bit=(((1<<i)&n)==0)?0:1;
-            if(bit!=isOne)  return false;
-            isOne=!isOne;
+        // int i=31;
+        // for(i;i>=0;i--){
+        //     int bit=(((1<<i)&n)==0)?0:1;
+        //     if(bit==1)    break;
+        // }
+        // bool isOne=1;
+        // for(i;i>=0;i--){
+        //     int bit=(((1<<i)&n)==0)?0:1;
+        //     if(bit!=isOne)  return false;
+        //     isOne=!isOne;
+        // }
+        // return true;
+
+        // int hsbit=log2(n);
+        // bitset<32>b(n);
+        // for(int i=0;i<=hsbit;i++){
+        //     if(b[i]==b[i+1])    return false;
+        // }
+        // return true;
+
+        int curbit=n%2;
+        n/=2;
+        while(n){
+            if(curbit==n%2)     return false;
+            curbit=n%2;
+            n/=2;
+
         }
         return true;
     }
