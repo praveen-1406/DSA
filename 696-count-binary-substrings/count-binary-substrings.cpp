c788ba98f171ad0278a15700bc38bdff0ac1ba64
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2026-02-19 14:33:24 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (32.17%) - LeetSync

--- 696-count-binary-substrings/count-binary-substrings.cpp
+++ 696-count-binary-substrings/count-binary-substrings.cpp
@@ -2,23 +2,37 @@ class Solution {
 public:
     int countBinarySubstrings(string s) {
         int count=0;
-        int zeros=0,prezeros=0,ones=0,preones=0;
+        // int zeros=0,prezeros=0,ones=0,preones=0;
+        // for(int i=0;i<s.size();i++){
+        //     if(s[i]=='0'){
+        //         if(ones!=0){
+        //             preones=ones;
+        //             ones=0;
+        //         }
+        //         zeros++;
+        //         if(preones>=zeros)  count++;
+        //     }
+        //     else if(s[i]=='1'){
+        //         if(zeros!=0){
+        //             prezeros=zeros;
+        //             zeros=0;
+        //         }
+        //         ones++;
+        //         if(prezeros>=ones)  count++;
+        //     }
+        // }
+        // return count;
+        char curchar=s[0];
+        int pre=0,cur=0;
         for(int i=0;i<s.size();i++){
-            if(s[i]=='0'){
-                if(ones!=0){
-                    preones=ones;
-                    ones=0;
-                }
-                zeros++;
-                if(preones>=zeros)  count++;
-            }
-            else if(s[i]=='1'){
-                if(zeros!=0){
-                    prezeros=zeros;
-                    zeros=0;
-                }
-                ones++;
-                if(prezeros>=ones)  count++;
+            if(s[i]==curchar){
+                cur++;
+                if(pre>=cur)    count++;
+            }else{
+                curchar=s[i];
+                pre=cur;
+                cur=1;
+                if(pre>=cur)    count++;
             }
         }
         return count;
