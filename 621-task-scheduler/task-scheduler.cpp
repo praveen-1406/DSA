91f853c7e7f3403a73c1ffeb63dfbac7a7479f46
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2026-03-05 23:49:36 +0530

    Time: 41 ms (25.82%) | Memory: 47.6 MB (38.32%) - LeetSync

---
+++ 621-task-scheduler/task-scheduler.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int>mp(26,0);
+        for(int i=0;i<tasks.size();i++){
+            mp[tasks[i]-'A']++;
+        }
+        int time=0;
+        priority_queue<int>pq;
+        for(int i=0;i<26;i++){
+            if(mp[i]>0){
+                pq.push(mp[i]);
+            }
+        }
+        while(!pq.empty()){
+            vector<int>temp;
+            for(int i=1;i<=n+1;i++){
+                if(!pq.empty()){
+                    int freq=pq.top();
+                    pq.pop();
+                    freq--;
+                    temp.push_back(freq);
+                }
+            }
+            for(int &f:temp){
+                if(f>0){
+                    pq.push(f);
+                }
+            }
+
+            if(pq.empty()){
+                time+=temp.size();
+            }else{
+                time+=n+1;
+            }
+        }
+        return time;
+    }
+};
