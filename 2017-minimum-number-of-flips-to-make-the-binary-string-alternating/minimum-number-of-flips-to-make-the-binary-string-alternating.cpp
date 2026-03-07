09402dd5ece47737a39481e90dad93feeec89030
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2026-03-07 23:41:33 +0530

    Time: 19 ms (57.20%) | Memory: 14.5 MB (76.80%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -3,25 +3,25 @@ public:
     int minFlips(string s) {
         int n=s.size();
         // s=s+s;
-        string s1="",s2="";
-        for(int i=0;i<2*n;i++){
-            if(i%2==0){
-                s1+='0';
-                s2+='1';
-            }else{
-                s1+='1';
-                s2+='0';
-            }
-        }
+        // string s1="",s2="";
+        // for(int i=0;i<2*n;i++){
+        //     if(i%2==0){
+        //         s1+='0';
+        //         s2+='1';
+        //     }else{
+        //         s1+='1';
+        //         s2+='0';
+        //     }
+        // }
         int result=INT_MAX;
         int flip1=0,flip2=0;
         int i=0,j=0;
         while(j<2*n){
-            if(s[j%n]!=s1[j]) flip1++;
-            if(s[j%n]!=s2[j]) flip2++;
+            if((s[j%n]-'0')!=(j%2)) flip1++;
+            if((s[j%n]-'0')!=(!(j%2))) flip2++;
             if(j-i+1>n){
-                if(s[i%n]!=s1[i]) flip1--;
-                if(s[i%n]!=s2[i]) flip2--;
+                if((s[i%n]-'0')!=(i%2)) flip1--;
+                if((s[i%n]-'0')!=(!(i%2))) flip2--;
                 i++;
             }
             if(j-i+1==n){
