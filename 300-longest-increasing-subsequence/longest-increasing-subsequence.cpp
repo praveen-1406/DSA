082085db0a71d3603ece41beacd08ee7b05a12a4
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2026-03-16 15:59:54 +0530

    Time: 319 ms (25.99%) | Memory: 14.3 MB (59.82%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -28,19 +28,35 @@ public:
         // vector<vector<int>>dp(n,vector<int>(n+1,-1));
         // return f(0,-1,nums,dp);
 
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        for(int i=0;i<=n;i++)   dp[n][i]=0;
+        // vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        // for(int i=0;i<=n;i++)   dp[n][i]=0;
+
+        // for(int idx=n-1;idx>=0;idx--){
+        //     for(int preIdx=n-1;preIdx>=-1;preIdx--){
+        //         int ans=0;
+        //         // not take
+        //         ans=max(ans,0+dp[idx+1][preIdx+1]);
+        //         // take
+        //         if(preIdx==-1 || nums[idx]>nums[preIdx])    ans=max(ans,1+dp[idx+1][idx+1]);
+        //         dp[idx][preIdx+1] = ans;
+        //     }
+        // }
+        // return dp[0][0];
+
+        vector<int>next(n+1,0),cur(n+1,0);
+        // for(int i=0;i<=n;i++)   dp[n][i]=0;
 
         for(int idx=n-1;idx>=0;idx--){
             for(int preIdx=n-1;preIdx>=-1;preIdx--){
                 int ans=0;
                 // not take
-                ans=max(ans,0+dp[idx+1][preIdx+1]);
+                ans=max(ans,0+next[preIdx+1]);
                 // take
-                if(preIdx==-1 || nums[idx]>nums[preIdx])    ans=max(ans,1+dp[idx+1][idx+1]);
-                dp[idx][preIdx+1] = ans;
+                if(preIdx==-1 || nums[idx]>nums[preIdx])    ans=max(ans,1+next[idx+1]);
+                cur[preIdx+1] = ans;
             }
+            next=cur;
         }
-        return dp[0][0];
+        return next[0];
     }
 };
