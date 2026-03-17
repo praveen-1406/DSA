bb03e6d3d3cee2b341c7fc5067d3503c5e9b9301
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2026-03-18 02:06:24 +0530

    Time: 1900 ms (8.19%) | Memory: 651.3 MB (5.96%) - LeetSync

---
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -0,0 +1,36 @@
+class Solution {
+    bool check(string w1,string w2){
+        //w1 is predecessor or w2
+        int n=w1.size(),m=w2.size();
+        if(n+1!=m)  return false;
+        int i=0,j=0,cnt=0;
+        while(i<n && j<m){
+            if(w1[i]!=w2[j]){
+                cnt++;
+                i--;
+            }
+            if(cnt>1)   return false;
+            i++;
+            j++;
+        }
+        return true;
+    }
+public:
+    int longestStrChain(vector<string>& words) {
+        int n=words.size();
+        sort(words.begin(),words.end(),[](string w1,string w2){
+            return w1.size()<w2.size();
+        });
+        vector<int>dp(n,1);
+        int maxi=1;
+        for(int ind=0;ind<n;ind++){
+            for(int pre=0;pre<ind;pre++){
+                if(check(words[pre],words[ind]) && dp[pre]+1>dp[ind]){
+                    dp[ind]=dp[pre]+1;
+                }
+            }
+            maxi=max(maxi,dp[ind]);
+        }
+        return maxi;
+    }
+};
