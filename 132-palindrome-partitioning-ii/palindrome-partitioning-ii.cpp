e9e40ccdbec6ecf12e89ea4ee4f187f3dc2f0246
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2026-03-25 15:11:48 +0530

    Time: 1301 ms (44.16%) | Memory: 30.4 MB (21.43%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -37,7 +37,32 @@ public:
         int n=s.size();
         // vector<vector<int>>dp(n,vector<int>(n,-1));
         // return f(0,n-1,s,dp);
-        vector<int>dp(n,-1);
-        return f(0,s,dp)-1;
+        // vector<int>dp(n,-1);
+        // return f(0,s,dp)-1;
+
+        vector<int>dp(n+1,0);
+        dp[n]=0;
+        for(int i=n-1;i>=0;i--){
+            int minCost=INT_MAX;
+            string temp="";
+            for(int j=i;j<s.size();j++){
+                temp+=s[j];
+                if(isPalindrome(temp)){
+                    int cost=1+dp[j+1];
+                    minCost=min(minCost,cost);
+                }
+            }
+            dp[i] = minCost;
+        }
+        return dp[0]-1;
+
     }
 };
+
+
+
+
+
+
+
+
