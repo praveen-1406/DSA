a9147e63fbaf9ebd6d0cb764334975260bc69043
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2026-03-27 22:44:13 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (91.30%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
@@ -4,19 +4,32 @@ public:
         int n=mat.size();
         int m=mat[0].size();
         k%=m;
-        vector<vector<int>>v=mat;
+        // vector<vector<int>>v=mat;
+        // for(int i=0;i<n;i++){
+        //     if(i%2==0){
+        //         reverse(v[i].begin(),v[i].begin()+k);
+        //         reverse(v[i].begin()+k,v[i].end());
+        //         reverse(v[i].begin(),v[i].end());
+        //     }else{
+        //         reverse(v[i].begin(),v[i].end());
+        //         reverse(v[i].begin(),v[i].begin()+k);
+        //         reverse(v[i].begin()+k,v[i].end());
+        //     }
+        // }
+        // if(mat==v)  return true;
+        // else    return false;
         for(int i=0;i<n;i++){
-            if(i%2==0){
-                reverse(v[i].begin(),v[i].begin()+k);
-                reverse(v[i].begin()+k,v[i].end());
-                reverse(v[i].begin(),v[i].end());
-            }else{
-                reverse(v[i].begin(),v[i].end());
-                reverse(v[i].begin(),v[i].begin()+k);
-                reverse(v[i].begin()+k,v[i].end());
+            for(int j=0;j<m;j++){
+                int cur=j;
+                int future;
+                if(i%2==0){
+                    future=(j+k)%m;
+                }else{
+                    future=(j-k+m)%m;
+                }
+                if(mat[i][cur]!=mat[i][future])   return false;
             }
         }
-        if(mat==v)  return true;
-        else    return false;
+        return true;
     }
 };
