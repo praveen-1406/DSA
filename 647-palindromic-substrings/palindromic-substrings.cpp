584db35fd75024f4a7a807b80008d1a046eb6301
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2026-04-01 03:29:34 +0530

    Time: 3 ms (87.31%) | Memory: 8.5 MB (77.25%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -10,6 +10,16 @@ class Solution {
     //     }
     //     return t[i][j]= false;
     // }
+
+    int count;
+
+    void check(string &s,int i,int j,int n){
+        while(i>=0 && j<n && s[i]==s[j]){
+            count++;
+            i--;
+            j++;
+        }
+    }
 public:
     int countSubstrings(string s) {
         int n=s.size();
@@ -25,18 +35,24 @@ public:
         // }
         // return count;
 
-        vector<vector<bool>>t(n,vector<bool>(n,false));
-        int count=0;
-        for(int L=1;L<=n;L++){
-            for(int i=0;i+L-1<n;i++){
-                int j=i+L-1;
-                if(i==j)    t[i][j]=true;
-                else if(i+1==j) t[i][j]=(s[i]==s[j]);
-                else{
-                    t[i][j]=(s[i]==s[j] && t[i+1][j-1]);
-                }
-                if(t[i][j])     count++;
-            }
+        // vector<vector<bool>>t(n,vector<bool>(n,false));
+        // int count=0;
+        // for(int L=1;L<=n;L++){
+        //     for(int i=0;i+L-1<n;i++){
+        //         int j=i+L-1;
+        //         if(i==j)    t[i][j]=true;
+        //         else if(i+1==j) t[i][j]=(s[i]==s[j]);
+        //         else{
+        //             t[i][j]=(s[i]==s[j] && t[i+1][j-1]);
+        //         }
+        //         if(t[i][j])     count++;
+        //     }
+        // }
+        // return count;
+        count=0;
+        for(int i=0;i<n;i++){
+            check(s,i,i,n);
+            check(s,i,i+1,n);
         }
         return count;
     }
