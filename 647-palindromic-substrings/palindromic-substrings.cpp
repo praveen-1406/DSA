4cb8c58485c3a1f38d84e17f37ead17d847efc3a
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2026-04-01 02:38:19 +0530

    Time: 528 ms (12.37%) | Memory: 8.4 MB (90.97%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    bool check(string &s,int i,int j){
+        if(i>j){
+            return true;
+        }
+        if(s[i]==s[j]){
+            return check(s,i+1,j-1);
+        }
+        return false;
+    }
+public:
+    int countSubstrings(string s) {
+        int n=s.size();
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(check(s,i,j)){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
