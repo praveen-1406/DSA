8060bcd5c639d8069bd6608c082c6d5263a370ae
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2026-04-05 02:32:28 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (22.70%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -5,11 +5,11 @@ public:
         int n=nums.size();
         int maxi=INT_MIN;
         for(int i=0;i<n;i++){
+            if(prefix==0)   prefix=1;
+            if(suffix==0)   suffix=1;
             prefix*=nums[i];
             suffix*=nums[n-i-1];
             maxi=max(maxi,max(prefix,suffix));
-            if(prefix==0)   prefix=1;
-            if(suffix==0)   suffix=1;
         }
         return maxi;
     }
