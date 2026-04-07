359180ada185b486ffa74a9e2d95447cea3caba5
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2026-04-08 00:07:12 +0530

    Time: 34 ms (52.59%) | Memory: 128.2 MB (41.38%) - LeetSync

---
+++ 2178-walking-robot-simulation-ii/walking-robot-simulation-ii.cpp
@@ -0,0 +1,50 @@
+class Robot {
+private:
+    bool moved=false;
+    int idx=0;
+    vector<int>dir;
+    vector<pair<int,int>>pos;
+    unordered_map<int,string>dirMp={{0,"East"},{1,"North"},{2,"West"},{3,"South"}};
+public:
+    Robot(int width, int height) {
+        for(int i=0;i<width;i++){
+            pos.emplace_back(i,0);
+            dir.push_back(0);
+        }
+        for(int i=1;i<height;i++){
+            pos.emplace_back(width-1,i);
+            dir.push_back(1);
+        }
+        for(int i=width-2;i>=0;i--){
+            pos.emplace_back(i,height-1);
+            dir.push_back(2);
+        }
+        for(int i=height-2;i>0;i--){
+            pos.emplace_back(0,i);
+            dir.push_back(3);
+        }
+        dir[0]=3;
+    }
+    
+    void step(int num) {
+        moved=true;
+        idx=(idx+num)%(pos.size());    
+    }
+    
+    vector<int> getPos() {
+        return {pos[idx].first,pos[idx].second};
+    }
+    
+    string getDir() {
+        if(!moved)  return "East";
+        return dirMp[dir[idx]];
+    }
+};
+
+/**
+ * Your Robot object will be instantiated and called as such:
+ * Robot* obj = new Robot(width, height);
+ * obj->step(num);
+ * vector<int> param_2 = obj->getPos();
+ * string param_3 = obj->getDir();
+ */
