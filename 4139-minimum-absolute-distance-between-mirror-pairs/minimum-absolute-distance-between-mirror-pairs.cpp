404f1af5be76848fab2bfbae6fcc589b8454b7c6
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2026-04-18 00:30:24 +0530

    Time: 106 ms (75.74%) | Memory: 131.9 MB (75.49%) - LeetSync

--- 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.cpp
+++ 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.cpp
@@ -9,28 +9,39 @@ class Solution {
     }
 public:
     int minMirrorPairDistance(vector<int>& nums) {
-        unordered_map<int,vector<int>>mp;
-        vector<int>rnums;
+        // unordered_map<int,vector<int>>mp;
+        // vector<int>rnums;
         int n=nums.size();
-        for(int i=0;i<n;i++){
-            mp[nums[i]].push_back(i);
-            rnums.push_back(rev(nums[i]));
-        }
+        // for(int i=0;i<n;i++){
+        //     mp[nums[i]].push_back(i);
+        //     rnums.push_back(rev(nums[i]));
+        // }
+        // int ans=INT_MAX;
+        // for(int i=0;i<n;i++){
+        //     vector<int>&v=mp[rnums[i]];
+        //     if(v.empty())   continue;
+        //     int m=v.size();
+        //     int l=0,r=m-1;
+        //     while(l<=r){
+        //         int mid=l+(r-l)/2;
+        //         if(v[mid]>i){
+        //             ans=min(ans,abs(v[mid]-i));
+        //             r=mid-1;
+        //         }else{
+        //             l=mid+1;
+        //         }
+        //     }
+        // }
+        // if(ans==INT_MAX)    return -1;
+        // return ans;
+
+        unordered_map<int,int>mp;
         int ans=INT_MAX;
         for(int i=0;i<n;i++){
-            vector<int>&v=mp[rnums[i]];
-            if(v.empty())   continue;
-            int m=v.size();
-            int l=0,r=m-1;
-            while(l<=r){
-                int mid=l+(r-l)/2;
-                if(v[mid]>i){
-                    ans=min(ans,abs(v[mid]-i));
-                    r=mid-1;
-                }else{
-                    l=mid+1;
-                }
+            if(mp.find(nums[i])!=mp.end()){
+                ans=min(ans,i-mp[nums[i]]);
             }
+            mp[rev(nums[i])]=i;
         }
         if(ans==INT_MAX)    return -1;
         return ans;
