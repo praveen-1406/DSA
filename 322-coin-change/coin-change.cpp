3746739ecc282795c483678f0ee75a54ee335042
Praveen Kumar Prajapati <124445308+praveen-1406@users.noreply.github.com>
2026-05-22 16:46:35 +0530

    Time: 19 ms (78.70%) | Memory: 18 MB (66.86%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,99 +1,30 @@
 class Solution {
-// Memoization
-    // int f(int amt,vector<int>& coins,vector<int>&dp){
-    //     if(amt==0)  return 0;
-    //     if(dp[amt]!=-1)     return dp[amt];
-    //     int mincnt=20000;
-    //     for(int i=0;i<coins.size();i++){
-    //         if(amt>=coins[i])    mincnt=min(mincnt,1+f(amt-coins[i],coins,dp));
-    //     }
-    //     return dp[amt]=mincnt;
-    // }
-
-    // int f(int ind,int tar,vector<int>&coins,vector<vector<int>>&dp){
-    //     if(ind==0){
-    //         if(tar%coins[0]==0)     return tar/coins[0];
-    //         else return 1e9;
-    //     }
-    //     if(dp[ind][tar]!=-1)    return dp[ind][tar];
-    //     int notPick=0+f(ind-1,tar,coins,dp);
-    //     int pick=INT_MAX;
-    //     if(tar>=coins[ind])     pick=1+f(ind,tar-coins[ind],coins,dp);
-    //     return dp[ind][tar]=min(notPick,pick);
-    // }
-
+    int f(vector<int>&coins,int amount,vector<int>&dp){
+        if(amount==0)   return 0;
+        if(dp[amount]!=-1)  return dp[amount];
+        int mini=INT_MAX;
+        for(auto coin:coins){
+            if(coin<=amount)    mini=min(mini,f(coins,amount-coin,dp));
+        }
+        if(mini==INT_MAX || mini==-1)  return dp[amount]= -1;
+        return dp[amount]= 1+mini;
+    }
 public:
     int coinChange(vector<int>& coins, int amount) {
-
-        // vector<int>dp(amount+1,-1);
-        // int ans=f(amount,coins,dp);
-        // if(ans>=20000)    return -1;
-        // return ans;
-
-
-        // vector<vector<int>>dp(amount+1,vector<int>(amount+1,0));    // cnt,amt
-        // for(int i=0;i<=amount;i++)  dp[i][0]=i;
-
-        // for(int cnt=amount;cnt>=0;cnt--){
-        //     for(int amt=1;amt<=amount;amt++){
-        //         int mincnt=INT_MAX;
-        //         for(int i=0;i<coins.size();i++){
-        //             if(amt>=coins[i])    mincnt=min(mincnt,dp[cnt+1][amt-coins[i]]);
-        //         }
-        //         dp[cnt][amt]=mincnt;
-        //     }
-        // }
-        // return dp[0][amount];
-
-// Space Optimization
-        // vector<int>dp(amount+1,0);
-
-        // for(int amt=1;amt<=amount;amt++){
-        //     int mincnt=20000;
-        //     for(int i=0;i<coins.size();i++){
-        //         if(amt>=coins[i])    mincnt=min(mincnt,1+dp[amt-coins[i]]);
-        //     }
-        //     dp[amt]=mincnt;
-        // }
-        // if(dp[amount]>=20000)    return -1;
-        // return dp[amount];
-
         int n=coins.size();
-        // vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        // int ans=f(n-1,amount,coins,dp);
-        // if(ans>=1e9)    return -1;
-        // return ans;
+        // vector<int>dp(amount+1,-1);
+        // return f(coins,amount,dp);
 
-        vector<vector<int>>dp(n,vector<int>(amount+1,0));
-        for(int t=0;t<=amount;t++) {
-            if(t%coins[0]==0)   dp[0][t]=t/coins[0];
-            else dp[0][t]=1e9;
-        }
-        for(int ind=1;ind<n;ind++){
-            for(int tar=0;tar<=amount;tar++){
-                int notPick=0+dp[ind-1][tar];
-                int pick=INT_MAX;
-                if(tar>=coins[ind])     pick=1+dp[ind][tar-coins[ind]];
-                dp[ind][tar]=min(notPick,pick);
+        vector<int>dp(amount+1,0);
+        for(int amt=1;amt<=amount;amt++){
+            int mini=INT_MAX;
+            for(auto coin:coins){
+                if(coin<=amt)    mini=min(mini,dp[amt-coin]);
             }
+            if(mini==INT_MAX)    dp[amt]=INT_MAX;
+            else    dp[amt]=1+mini;
         }
-
-        if(dp[n-1][amount]>=1e9)    return -1;
-        return dp[n-1][amount];
-
+        if(dp[amount]==INT_MAX)     return -1;
+        return dp[amount];
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
